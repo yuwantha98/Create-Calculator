28d5a509879b416ee783bb11ada2b0d56741473e
yuwantha98 <yuwantharavihara123@gmail.com>
2024-06-19 10:30:23 +0530

    Add selector operator

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 function cal() {
   let num1 = parseInt(document.getElementById("no1").value);
   let num2 = parseInt(document.getElementById("no2").value);
+  let operator = document.getElementById("cars").value;
+  let result;
 
-  let sum = num1 + num2;
+  switch (operator) {
+    case "+":
+      result = num1 + num2;
+      break;
+    case "-":
+      result = num1 - num2;
+      break;
+    case "*":
+      result = num1 * num2;
+      break;
+    case "/":
+      if (num2 !== 0) {
+        result = num1 / num2;
+      } else {
+        result = "Error: Division by zero";
+      }
+      break;
+    default:
+      result = "Invalid operator";
+  }
 
-  document.getElementById("output").innerText = parseInt(sum);
+  document.getElementById("output").innerText = parseInt(result);
 }
